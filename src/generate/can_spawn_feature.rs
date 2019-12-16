eed243fad11cbaea7ab6c08ea66893a834792b6c
Maciej Srokowski <srokowski.maciej@gmail.com>
2019-12-16 21:06:45 +0100

    Add non zero priority can spawn feature test

--- src/generate/can_spawn_feature.rs
+++ src/generate/can_spawn_feature.rs
@@ -24,6 +24,8 @@ pub fn can_spawn_feature(
                 + obstacle.velocity * (time_travelled - obstacle.spawn_time);
 
             let prefab_world_bounds_toi = world.world_bounds
+                // This is required because sometimes, due to priority, prefab spawn position is outside world bounds
+                .merged(&prefab.bounding_box.transform_by(&Isometry3::new(prefab_spawn_position, nalgebra::zero())))
                 .toi_with_ray(
                     &Isometry3::new(nalgebra::zero(), nalgebra::zero()),
                     &Ray::new(Point3::origin() + prefab_spawn_position, prefab.velocity),
@@ -39,10 +41,11 @@ pub fn can_spawn_feature(
             println!("Prefab spawn position: {:?}", prefab_spawn_position);
             println!("Obstacle spawn position: {:?}", obstacle_spawn_position);
             println!("Prefab toi: {}", prefab_world_bounds_toi);
+
             // TODO In reality this is a fix for a bug in query::time_of_impact
             if prefab.bounding_box.transform_by(&Isometry3::new(prefab_spawn_position, nalgebra::zero()))
                 .intersects(&obstacle.bounding_box.transform_by(&Isometry3::new(obstacle_spawn_position, nalgebra::zero()))) {
-                return false
+                return false;
             }
 
             let time_of_impact = query::time_of_impact(
@@ -75,163 +78,211 @@ mod tests {
     use std::collections::VecDeque;
     use std::iter::FromIterator;
 
-    #[test]
-    fn test_can_spawn_feature_collision() {
-        let prefab = Prefab {
-            prefab_id: 0,
-            position: Vector3::new(0., 0., 0.),
-            bounding_box: AABB::from_half_extents(Point3::new(0., 0., 0.), Vector3::new(0.5, 0.5, 0.5)),
-            velocity: Vector3::new(0., -1., 0.),
-        };
-        let feature = Feature {
-            translate_x: false,
-            translate_z: false,
-            prefabs: &[prefab],
-            spawn_count: 1,
-            spawns_per_second: 1.,
-            trigger_position: 10.,
-            priority: 0,
-        };
-        let obstacle = CollideableEntity {
-            spawn_position: Vector3::new(0., 8., 5.),
-            prefab_id: 0,
-            bounding_box: AABB::from_half_extents(Point3::new(0., 0., 0.), Vector3::new(0.5, 0.5, 0.5)),
-            velocity: Vector3::new(0., -1., -1.),
-            spawn_time: 0.0,
-            priority: 0,
-        };
-        let world = VisibleWorld {
-            world_bounds: AABB::from_half_extents(Point3::new(0., 0., 0.), Vector3::new(10., 10., 10.)),
-            travel_speed: 4.0,
-            spawn_barrier: 8.0,
-        };
-        let can_spawn = can_spawn_feature(
-            &feature,
-            &VecDeque::from_iter([obstacle].iter().cloned()),
-            &world,
-            0.,
-            &Vector3::new(0., 0., 0.),
-        );
-        assert_eq!(can_spawn, false);
+    mod zero_travel_time {
+        use super::*;
+
+        #[test]
+        fn test_can_spawn_feature_collision() {
+            let prefab = Prefab {
+                prefab_id: 0,
+                position: Vector3::new(0., 0., 0.),
+                bounding_box: AABB::from_half_extents(Point3::new(0., 0., 0.), Vector3::new(0.5, 0.5, 0.5)),
+                velocity: Vector3::new(0., -1., 0.),
+            };
+            let feature = Feature {
+                translate_x: false,
+                translate_z: false,
+                prefabs: &[prefab],
+                spawn_count: 1,
+                spawns_per_second: 1.,
+                trigger_position: 10.,
+                priority: 0,
+            };
+            let obstacle = CollideableEntity {
+                spawn_position: Vector3::new(0., 8., 5.),
+                prefab_id: 0,
+                bounding_box: AABB::from_half_extents(Point3::new(0., 0., 0.), Vector3::new(0.5, 0.5, 0.5)),
+                velocity: Vector3::new(0., -1., -1.),
+                spawn_time: 0.0,
+                priority: 0,
+            };
+            let world = VisibleWorld {
+                world_bounds: AABB::from_half_extents(Point3::new(0., 0., 0.), Vector3::new(10., 10., 10.)),
+                travel_speed: 4.0,
+                spawn_barrier: 8.0,
+            };
+            let can_spawn = can_spawn_feature(
+                &feature,
+                &VecDeque::from_iter([obstacle].iter().cloned()),
+                &world,
+                0.,
+                &Vector3::new(0., 0., 0.),
+            );
+            assert_eq!(can_spawn, false);
+        }
+
+        #[test]
+        fn test_can_spawn_feature_collision_catch_up() {
+            let prefab = Prefab {
+                prefab_id: 0,
+                position: Vector3::new(0., 0., 0.),
+                bounding_box: AABB::from_half_extents(Point3::new(0., 0., 0.), Vector3::new(0.5, 0.5, 0.5)),
+                velocity: Vector3::new(0., -1., 0.),
+            };
+            let feature = Feature {
+                translate_x: false,
+                translate_z: false,
+                prefabs: &[prefab],
+                spawn_count: 1,
+                spawns_per_second: 1.,
+                trigger_position: 10.,
+                priority: 0,
+            };
+            let obstacle = CollideableEntity {
+                spawn_position: Vector3::new(0., -2.25, 0.),
+                prefab_id: 0,
+                bounding_box: AABB::from_half_extents(Point3::new(0., 0., 0.), Vector3::new(0.5, 0.5, 0.5)),
+                velocity: Vector3::new(0., -0.5, -0.),
+                spawn_time: 0.0,
+                priority: 0,
+            };
+            let world = VisibleWorld {
+                world_bounds: AABB::from_half_extents(Point3::new(0., 0., 0.), Vector3::new(10., 10., 10.)),
+                travel_speed: 4.0,
+                spawn_barrier: 8.0,
+            };
+            let can_spawn = can_spawn_feature(
+                &feature,
+                &VecDeque::from_iter([obstacle].iter().cloned()),
+                &world,
+                0.,
+                &Vector3::new(0., 0., 0.),
+            );
+            assert_eq!(can_spawn, false);
+        }
+
+        #[test]
+        fn test_can_spawn_feature_no_collision_catch_up() {
+            let prefab = Prefab {
+                prefab_id: 0,
+                position: Vector3::new(0., 0., 0.),
+                bounding_box: AABB::from_half_extents(Point3::new(0., 0., 0.), Vector3::new(0.5, 0.5, 0.5)),
+                velocity: Vector3::new(0., -1., 0.),
+            };
+            let feature = Feature {
+                translate_x: false,
+                translate_z: false,
+                prefabs: &[prefab],
+                spawn_count: 1,
+                spawns_per_second: 1.,
+                trigger_position: 10.,
+                priority: 0,
+            };
+            let obstacle = CollideableEntity {
+                spawn_position: Vector3::new(0., -2.5, 0.),
+                prefab_id: 0,
+                bounding_box: AABB::from_half_extents(Point3::new(0., 0., 0.), Vector3::new(0.5, 0.5, 0.5)),
+                velocity: Vector3::new(0., -0.5, -0.),
+                spawn_time: 0.0,
+                priority: 0,
+            };
+            let world = VisibleWorld {
+                world_bounds: AABB::from_half_extents(Point3::new(0., 0., 0.), Vector3::new(10., 10., 10.)),
+                travel_speed: 4.0,
+                spawn_barrier: 8.0,
+            };
+            let can_spawn = can_spawn_feature(
+                &feature,
+                &VecDeque::from_iter([obstacle].iter().cloned()),
+                &world,
+                0.,
+                &Vector3::new(0., 0., 0.),
+            );
+            assert_eq!(can_spawn, true);
+        }
+
+        #[test]
+        fn test_can_spawn_feature_collision_intersecting_on_spawn() {
+            let prefab = Prefab {
+                prefab_id: 0,
+                position: Vector3::new(0., 0., 0.),
+                bounding_box: AABB::from_half_extents(Point3::new(0., 0., 0.), Vector3::new(0.5, 0.5, 0.5)),
+                velocity: Vector3::new(0., -1., 0.),
+            };
+            let feature = Feature {
+                translate_x: false,
+                translate_z: false,
+                prefabs: &[prefab],
+                spawn_count: 1,
+                spawns_per_second: 1.,
+                trigger_position: 10.,
+                priority: 0,
+            };
+            let obstacle = CollideableEntity {
+                spawn_position: Vector3::new(0., 8.0, 0.),
+                prefab_id: 0,
+                bounding_box: AABB::from_half_extents(Point3::new(0., 0., 0.), Vector3::new(0.5, 0.5, 0.5)),
+                velocity: Vector3::new(0., -1.0, -0.),
+                spawn_time: 0.0,
+                priority: 0,
+            };
+            let world = VisibleWorld {
+                world_bounds: AABB::from_half_extents(Point3::new(0., 0., 0.), Vector3::new(10., 10., 10.)),
+                travel_speed: 4.0,
+                spawn_barrier: 8.0,
+            };
+            let can_spawn = can_spawn_feature(
+                &feature,
+                &VecDeque::from_iter([obstacle].iter().cloned()),
+                &world,
+                0.,
+                &Vector3::new(0., 0., 0.),
+            );
+            assert_eq!(can_spawn, false);
+        }
     }
 
-    #[test]
-    fn test_can_spawn_feature_collision_catch_up() {
-        let prefab = Prefab {
-            prefab_id: 0,
-            position: Vector3::new(0., 0., 0.),
-            bounding_box: AABB::from_half_extents(Point3::new(0., 0., 0.), Vector3::new(0.5, 0.5, 0.5)),
-            velocity: Vector3::new(0., -1., 0.),
-        };
-        let feature = Feature {
-            translate_x: false,
-            translate_z: false,
-            prefabs: &[prefab],
-            spawn_count: 1,
-            spawns_per_second: 1.,
-            trigger_position: 10.,
-            priority: 0,
-        };
-        let obstacle = CollideableEntity {
-            spawn_position: Vector3::new(0., -2.25, 0.),
-            prefab_id: 0,
-            bounding_box: AABB::from_half_extents(Point3::new(0., 0., 0.), Vector3::new(0.5, 0.5, 0.5)),
-            velocity: Vector3::new(0., -0.5, -0.),
-            spawn_time: 0.0,
-            priority: 0,
-        };
-        let world = VisibleWorld {
-            world_bounds: AABB::from_half_extents(Point3::new(0., 0., 0.), Vector3::new(10., 10., 10.)),
-            travel_speed: 4.0,
-            spawn_barrier: 8.0,
-        };
-        let can_spawn = can_spawn_feature(
-            &feature,
-            &VecDeque::from_iter([obstacle].iter().cloned()),
-            &world,
-            0.,
-            &Vector3::new(0., 0., 0.),
-        );
-        assert_eq!(can_spawn, false);
-    }
+    mod non_zero_travel_time_with_shift {
+        use super::*;
 
-    #[test]
-    fn test_can_spawn_feature_no_collision_catch_up() {
-        let prefab = Prefab {
-            prefab_id: 0,
-            position: Vector3::new(0., 0., 0.),
-            bounding_box: AABB::from_half_extents(Point3::new(0., 0., 0.), Vector3::new(0.5, 0.5, 0.5)),
-            velocity: Vector3::new(0., -1., 0.),
-        };
-        let feature = Feature {
-            translate_x: false,
-            translate_z: false,
-            prefabs: &[prefab],
-            spawn_count: 1,
-            spawns_per_second: 1.,
-            trigger_position: 10.,
-            priority: 0,
-        };
-        let obstacle = CollideableEntity {
-            spawn_position: Vector3::new(0., -2.5, 0.),
-            prefab_id: 0,
-            bounding_box: AABB::from_half_extents(Point3::new(0., 0., 0.), Vector3::new(0.5, 0.5, 0.5)),
-            velocity: Vector3::new(0., -0.5, -0.),
-            spawn_time: 0.0,
-            priority: 0,
-        };
-        let world = VisibleWorld {
-            world_bounds: AABB::from_half_extents(Point3::new(0., 0., 0.), Vector3::new(10., 10., 10.)),
-            travel_speed: 4.0,
-            spawn_barrier: 8.0,
-        };
-        let can_spawn = can_spawn_feature(
-            &feature,
-            &VecDeque::from_iter([obstacle].iter().cloned()),
-            &world,
-            0.,
-            &Vector3::new(0., 0., 0.),
-        );
-        assert_eq!(can_spawn, true);
-    }
-
-    #[test]
-    fn test_can_spawn_feature_collision_intersecting_on_spawn() {
-        let prefab = Prefab {
-            prefab_id: 0,
-            position: Vector3::new(0., 0., 0.),
-            bounding_box: AABB::from_half_extents(Point3::new(0., 0., 0.), Vector3::new(0.5, 0.5, 0.5)),
-            velocity: Vector3::new(0., -1., 0.),
-        };
-        let feature = Feature {
-            translate_x: false,
-            translate_z: false,
-            prefabs: &[prefab],
-            spawn_count: 1,
-            spawns_per_second: 1.,
-            trigger_position: 10.,
-            priority: 0,
-        };
-        let obstacle = CollideableEntity {
-            spawn_position: Vector3::new(0., 8.0, 0.),
-            prefab_id: 0,
-            bounding_box: AABB::from_half_extents(Point3::new(0., 0., 0.), Vector3::new(0.5, 0.5, 0.5)),
-            velocity: Vector3::new(0., -1.0, -0.),
-            spawn_time: 0.0,
-            priority: 0,
-        };
-        let world = VisibleWorld {
-            world_bounds: AABB::from_half_extents(Point3::new(0., 0., 0.), Vector3::new(10., 10., 10.)),
-            travel_speed: 4.0,
-            spawn_barrier: 8.0,
-        };
-        let can_spawn = can_spawn_feature(
-            &feature,
-            &VecDeque::from_iter([obstacle].iter().cloned()),
-            &world,
-            0.,
-            &Vector3::new(0., 0., 0.),
-        );
-        assert_eq!(can_spawn, false);
+        #[test]
+        fn test_can_spawn_feature_collision() {
+            let prefab = Prefab {
+                prefab_id: 0,
+                position: Vector3::new(0., 0., 0.),
+                bounding_box: AABB::from_half_extents(Point3::new(0., 0., 0.), Vector3::new(0.5, 0.5, 0.5)),
+                velocity: Vector3::new(0., -1., 0.),
+            };
+            let feature = Feature {
+                translate_x: false,
+                translate_z: false,
+                prefabs: &[prefab],
+                spawn_count: 1,
+                spawns_per_second: 1.,
+                trigger_position: 10.,
+                priority: 5,
+            };
+            let obstacle = CollideableEntity {
+                spawn_position: Vector3::new(0., 8., 5.),
+                prefab_id: 0,
+                bounding_box: AABB::from_half_extents(Point3::new(0., 0., 0.), Vector3::new(0.5, 0.5, 0.5)),
+                velocity: Vector3::new(0., -0.5, -0.5),
+                spawn_time: 5.0,
+                priority: 0,
+            };
+            let world = VisibleWorld {
+                world_bounds: AABB::from_half_extents(Point3::new(0., 0., 0.), Vector3::new(10., 10., 10.)),
+                travel_speed: 4.0,
+                spawn_barrier: 8.0,
+            };
+            let can_spawn = can_spawn_feature(
+                &feature,
+                &VecDeque::from_iter([obstacle].iter().cloned()),
+                &world,
+                5.,
+                &Vector3::new(0., 0., 0.),
+            );
+            assert_eq!(can_spawn, false);
+        }
     }
 }
